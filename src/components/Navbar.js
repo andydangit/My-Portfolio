1169924ff0b3786cd86875ecadcad4b5356aef84
Andy Dang <andy.dang.it@gmail.com>
2021-07-20 20:15:49 -0500

    Add React router navbar menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,49 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <>
-        <nav className="navbar">
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className="navbar">
         <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo">
+            {/* A icon  */}
             My Portfolio <i class="fab fa-adn"></i>
-        </Link>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-lins" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-lins" onClick={closeMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/project" className="nav-lins" onClick={closeMenu}>
+                Project
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-lins" onClick={closeMenu}>
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
-        </nav>
-        </>
-    )
+      </nav>
+    </>
+  );
 }
 
-export default Navbar
+export default Navbar;
