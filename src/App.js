8c71c685cd4ef68baef41c95ab5f3e2a55d20830
Andy Dang <andy.dang.it@gmail.com>
2021-07-21 18:14:02 -0500

    add sampel on home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import './App.css';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Router> 
-    <Navbar />
-    <Switch>
-      <Route path='/' exact />
-      </Switch> 
-    </Router>
-
-    </> 
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+      Full Stack Developer <p></p>
+      <p> Andy Dang</p>
+      <p></p>I build and design apps that focus on the user experience
+    </>
   );
 }
 
