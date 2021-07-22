bebac8c098406ceac131e0ad4bdcf012d9acd9c6
pandoragear <andy.dang.it@gmail.com>
2021-07-21 20:53:13 -0500

    creating projects function

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button } from './Button';
 
 function Projects() {
-    return (
-        <div className="projects-container">
-            
-        </div>
-    )
+  return (
+    <div className="projects-container">
+      <h1> Projects </h1>
+      <p> This is my project </p>
+      <div className="projects-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Projects xx
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
