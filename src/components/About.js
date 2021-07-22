2592cd6657efe89918e86c9b6832ff57e70b2b4d
Andy Dang <andy.dang.it@gmail.com>
2021-07-21 19:06:11 -0500

    add text to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,9 @@ import React from 'react'
 function About() {
     return (
         <div className="about-container">
+            <h1> About </h1>
+            <p> My name is Andy Dang and I am am inspiring to be a Full Stack devloper by show casing my skill with this portfolio and other project </p> 
+
             
             
         </div>
