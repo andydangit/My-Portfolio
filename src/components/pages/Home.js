64e052406ae004627015238edb81488ef467e921
pandoragear <andy.dang.it@gmail.com>
2021-07-21 21:09:07 -0500

    create home page function

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,1 +1,14 @@
-import './app.css'
+import React from 'react';
+import "./app.css";
+import Projects from "../Projects";
+
+function Home() {
+  return (
+    <>
+      <Projects />
+    </>
+  );
+}
+
+
+export default Home;
