c53701116fec0421fd2387d4b73890f45aaa7ba7
Andy Dang <andy.dang.it@gmail.com>
2021-07-23 14:33:48 -0500

    update info for About page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,27 @@
-import React from 'react'
-
+import React from "react";
 
 function About() {
-    return (
-        <div>
-         <h1> Hey, I'm Andy. </h1>
-         <p> My goal is to be a full stack developer</p> 
+  return (
+    <div className="About_Container">
+      <div>
+        <h1> Hey, I'm Andy. </h1>
+        <p> My goal is to be a full stack developer</p>
       </div>
-      );
-    }
+      <div>
+        <div>
+            <h1> Skillset </h1> 
+            </div>
+            <tr> 
+                <li> HTML / CSS </li>
+                <li> JavaScript</li>
+                <li> React</li>
+                <li> MySQL</li>
+                <li> MongoDB</li>
+                <li> Express </li>
+            </tr>
+      </div>
+    </div>
+  );
+}
 
-export default About
-
-
+export default About;
