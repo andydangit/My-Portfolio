fc7ebc832301e0aea54244c1b145cf48e26a6b32
Andy Dang <andy.dang.it@gmail.com>
2021-07-22 21:24:43 -0500

    Remove test component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import "../../App.css";
 import Projects from "../Projects";
-import Cards from '../../components/Cards';
 
 function Home() {
   return (
     <>
       <Projects />
-      <Cards />
     </>
   );
 }
