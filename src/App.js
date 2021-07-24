f90d3df1f47629b08d9e157b312bb5916cb11da9
pandoragear <andy.dang.it@gmail.com>
2021-07-24 01:15:06 -0500

    Add updated component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import 'semantic-ui-css/semantic.min.css'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
+import Contact from './components/pages/Contact'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
@@ -13,6 +16,8 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/projects' component={Projects} />
         </Switch>
       </Router>
 
