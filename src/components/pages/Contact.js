dea190e67119ccf25bdcda5d53b489d0ad98fc08
pandoragear <andy.dang.it@gmail.com>
2021-07-25 09:36:07 -0500

    Add inital info for Contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,8 +3,17 @@ import React from 'react'
 function Contact() {
     return (
         <div>
-            
+            <div className="contact_container">
+            <h1 className="contact_title">Contact Me</h1> 
+            </div> 
+            <div> 
+                <p> Feel Free to reach out to me and I will get back with you. </p>
+            </div>
+            <div> 
+
+                </div>
         </div>
+
     )
 }
 
