9081e5631ed26c09d1e0e76abca7eb015a34b293
pandoragear <andy.dang.it@gmail.com>
2021-07-25 11:02:04 -0500

    Modify project 2 info

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -14,11 +14,11 @@ const Projects = () => (
   </Card>
 
     <Card>
-    <Image src='/images/project_1.png' wrapped ui={false} />
+    <Image src='/images/project_2.png' wrapped ui={false} />
     <Card.Content>
-      <Card.Header> <a href="https://www.google.com">  HTML / CSS / Bootstrap </a> </Card.Header>
+      <Card.Header> <a href="https://andydangit.github.io/Exercise_List/">  HTML / CSS / Javascript / Type.fit api  </a> </Card.Header>
       <Card.Description>
-        Create a live web application for a restaurant to showcase their business
+        SPA for Todo list and a daily motivational quote that pull data from Type.fit api
       </Card.Description>
     </Card.Content>
   </Card>
