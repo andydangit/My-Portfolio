0ca6fee0136cd2c0581052f5c0fd3e8b46ccf7c3
pandoragear <andy.dang.it@gmail.com>
2021-07-25 09:33:04 -0500

    Show extra card project on screen

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,18 +2,27 @@ import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react'
 
 const Projects = () => (
-  <Card>
-    <Image src='/images/project_1.png' wrapped ui={false} />
+  <div> 
+    <Card>
+    <Image src='/images/project_1.png'  wrapped ui={false} /> 
     <Card.Content>
       <Card.Header> <a href="https://www.google.com">  HTML / CSS / Bootstrap </a> </Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
       <Card.Description>
         Create a live web application for a restaurant to showcase their business
       </Card.Description>
     </Card.Content>
   </Card>
+
+    <Card>
+    <Image src='/images/project_1.png' wrapped ui={false} />
+    <Card.Content>
+      <Card.Header> <a href="https://www.google.com">  HTML / CSS / Bootstrap </a> </Card.Header>
+      <Card.Description>
+        Create a live web application for a restaurant to showcase their business
+      </Card.Description>
+    </Card.Content>
+  </Card>
+  </div> 
 )
 
 export default Projects
