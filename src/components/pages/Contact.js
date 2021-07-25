9699c810799be3a98db310606566ab98ebafffdb
pandoragear <andy.dang.it@gmail.com>
2021-07-25 16:34:10 -0500

    Add Button and info to Contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../../components/Projects.css';
+import { Button } from 'semantic-ui-react'
 
 function Contact() {
     return (
         <div>
+            <h1 className="projects-container"> Contact Me</h1> 
             <div className="contact_container">
-            <h1 className="contact_title">Contact Me</h1> 
-            </div> 
+            
+            
             <div> 
                 <p> Feel Free to reach out to me and I will get back with you. </p>
-            </div>
-            <div> 
-
-                </div>
+            </div >
+            <div className="contact_button"> 
+            <Button basic color='black'> <a href="mailto:andy.dang.it@gmail.com">Andy.dang.it@gmail.com </a></Button>
+            <Button basic color='black'> <a href="https://github.com/andydangit"> Github </a></Button>
+            <Button basic color='black'> <a href="https://www.linkedin.com/in/andy-dang-12693341/"> Linkedin </a></Button>
+            </div> 
+            </div> 
         </div>
 
     )
